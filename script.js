bdfa1a1f08c218f8a90c98d76914a942f801e9c4
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-03-09 18:35:18 -0500

    Added like and dislike buttons

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let redditLink = document.getElementById("homepage");
-redditLink.addEventListener("click", topOfPage);
+let count = 0;
 
-function topOfPage() {
-  $("html,body").scrollTop(0);
+let likeCount = document.getElementById("likeCount");
+let likeButton = document.getElementById("likeImageButton");
+let dislikeButton = document.getElementById("dislikeImageButton");
+
+function like(event) {
+  console.log(event);
+  count += 1;
+  likeCount.innerHTML = `${count}`;
+}
+
+function dislike() {
+  count -= 1;
+  likeCount.innerHTML = `${count}`;
 }
